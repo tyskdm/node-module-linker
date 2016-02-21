65f56dac45bcd5052e878238d146400469deee92
Tsuyoshi Kodama <tsuyoshi.kodama@gmail.com>
2016-02-21 09:21:22 +0900

    Revert "add publish command to npm."
    This reverts commit ccc176e1f6ec65670d306488c61f6604e324b41f.

--- jenkins-job/publish.sh
+++ jenkins-job/publish.sh
@@ -1,12 +1,3 @@
 ### publish.sh ###
-echo "== publish.sh =="
-pwd
-whoami
-source ~/.nvm/nvm.sh
-nvm install 0.10
-echo "== npm publish =="
-npm publish
-echo "== git push github =="
-
-
-
+echo "npm publish"
+echo "git push github"
